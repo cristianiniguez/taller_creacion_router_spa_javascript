f6847993f98a8d23ffc1cde03581a22ea0c8d165
Cristian Felix Iñiguez Porcel <felizpc1@gmail.com>
2020-08-15 21:46:50 -0400

    Haciendo match entre la URL y una ruta

--- router.js
+++ router.js
@@ -3,10 +3,20 @@ class Router {
         this.routes = routes;
         this._loadInitialRoutes();
     }
-}
-
-_loadInitialRoutes() {
-    const pathNameSplit = window.location.pathname.split('/');
-    const pathSegs = pathNameSplit.length > 1 ? pathNameSplit(1) : ''
-    this.loadRoute(...pathSegs)
+    _matchUrlToRoute(urlSegs) {
+        const matchedRoute = this.routes.find(route => {
+            const routePathSegs = route.path.split('/').slice(1)
+            if (routePathSegs.length !== urlSegs.length) {
+                return false;
+            }
+            return routePathSegs
+                .every((routePathSeg, i) => routePathSeg === urlSegs[i]);
+        })
+        return matchedRoute
+    }
+    _loadInitialRoute() {
+        const pathNameSplit = window.location.pathname.split('/');
+        const pathSegs = pathNameSplit.length > 1 ? pathNameSplit(1) : ''
+        this.loadRoute(...pathSegs);
+    }
 }
